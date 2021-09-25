2ce3c7d996004c1fcef1111903d5d682bc02be71
Mohamed Sharaf <sharaf.haxor@gmail.com>
2021-09-25 20:23:27 +0530

    Add student model

---
+++ student-svc/app/Models/Student.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
+class Student extends Model
+{
+    use HasApiTokens, HasFactory;
+
+    const STATUS_ACTIVE = 1;
+    const STATUS_BLOCKED = 0;
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
