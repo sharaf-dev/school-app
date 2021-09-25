8453b03a75c5158bb3fcf2eaa392c7431de78f50
Mohamed Sharaf <sharaf.haxor@gmail.com>
2021-09-25 20:24:49 +0530

    Add student factory

---
+++ student-svc/database/factories/StudentFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class StudentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Student::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'student_id' => 'f3d9be31-c993-480c-8ba6-c472cbe1cc46',
+            'name' => $this->faker->name,
+            'email' => 'student@test.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
+            'status' => Student::STATUS_ACTIVE
+        ];
+    }
+}
