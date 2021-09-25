959244d9b8839383a68f2b400bcca95a706e883d
Mohamed Sharaf <sharaf.haxor@gmail.com>
2021-09-25 20:32:02 +0530

    Add unit test for StudentRepository

---
+++ student-svc/tests/Unit/app/Repositories/StudentRepositoryTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Unit\app\Repositories;
+
+use Tests\TestCase;
+use App\Models\Student;
+use App\Repositories\StudentRepository;
+
+class StudentRepositoryTest extends TestCase
+{
+    public function test_GetStudent_returns_user()
+    {
+        $expected = Student::factory()->make();
+        $studentRepo = $this->createStudentRepository($expected);
+
+        $result = $studentRepo->getStudent([]);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_GetStudent_returns_null()
+    {
+        $studentRepo = $this->createStudentRepository();
+
+        $result = $studentRepo->getStudent([]);
+        $this->assertNull($result);
+    }
+
+    public function test_CheckPassword_returns_true()
+    {
+        $expected = Student::factory()->make();
+        $studentRepo = $this->createStudentRepository($expected);
+
+        $result = $studentRepo->checkPassword($expected, 'password');
+        $this->assertTrue($result);
+    }
+
+    public function test_CheckPassword_returns_false()
+    {
+        $expected = Student::factory()->make();
+        $studentRepo = $this->createStudentRepository($expected);
+
+        $result = $studentRepo->checkPassword($expected, 'incorrect password');
+        $this->assertFalse($result);
+    }
+
+    private function createStudentRepository(Student $expected = null)
+    {
+        $student = $this->mockStudent($expected);
+        return new StudentRepository($student);
+    }
+
+    private function mockStudent(Student $student = null) : Student
+    {
+        return $this->mock(Student::class, function ($mock) use ($student) {
+            $mock->shouldReceive('firstWhere')->andReturn($student);
+        });
+    }
+}
