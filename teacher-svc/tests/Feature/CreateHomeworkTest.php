03937edc13dd4108c434feb13a585b890e4b3a15
Mohamed Sharaf <sharaf.haxor@gmail.com>
2021-09-26 15:36:07 +0530

    Add integration test for create homework

---
+++ teacher-svc/tests/Feature/CreateHomeworkTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Homework;
+use App\Models\StudentHomework;
+
+class CreateHomeworkTest extends TestCase
+{
+    private string $uri = 'api/homework/create';
+
+    public function test_valid_create_homework_response()
+    {
+        $homework = Homework::factory()->make();
+        $inputs = [
+            'title' => $homework->title,
+            'description' => $homework->description,
+        ];
+
+        $expectedRecord = [
+            'title' => $homework->title,
+            'description' => $homework->description,
+            'teacher_id' => $homework->teacher_id,
+        ];
+
+        $expectedResponse = [
+            'status' => 'HOMEWORK_CREATED',
+            'message' => true,
+            'data' => [
+                'homework_id' => true
+            ]
+        ];
+
+
+        $this->setAuthorizationHeader();
+        $response = $this->withHeaders($this->headers)
+                        ->json('POST', $this->uri, $inputs);
+
+        $response->assertStatus(200);
+        $response->assertJson($expectedResponse);
+        $this->assertDatabaseHas('homeworks', $expectedRecord);
+    }
+
+    public function test_valid_create_homework_with_assignees_response()
+    {
+        $homework = Homework::factory()->make();
+        $studentId = 2;
+        $inputs = [
+            'title' => $homework->title,
+            'description' => $homework->description,
+            'assignees' => [$studentId],
+        ];
+
+        $expectedHomeworkRecord = [
+            'title' => $homework->title,
+            'description' => $homework->description,
+            'teacher_id' => $homework->teacher_id,
+        ];
+
+        $expectedStudentHomeworkRecord = [
+            'homework_id' => 1,
+            'student_id' => $studentId,
+            'status' => StudentHomework::STATUS_NEW,
+        ];
+
+        $expectedResponse = [
+            'status' => 'HOMEWORK_CREATED_ASSIGNED',
+            'message' => true,
+            'data' => [
+                'homework_id' => true,
+                'students' => [$studentId],
+            ]
+        ];
+
+
+        $this->setAuthorizationHeader();
+        $response = $this->withHeaders($this->headers)
+                        ->json('POST', $this->uri, $inputs);
+
+        $response->assertStatus(200);
+        $response->assertJson($expectedResponse);
+        $this->assertDatabaseHas('homeworks', $expectedHomeworkRecord);
+        $this->assertDatabaseHas('student_homework', $expectedStudentHomeworkRecord);
+    }
+}
--- teacher-svc/tests/TestCase.php
+++ teacher-svc/tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Models\Teacher;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -15,4 +16,27 @@ abstract class TestCase extends BaseTestCase
         Parent::Setup();
         $this->artisan('migrate:fresh --seed');
     }
+
+    public function setAuthorizationHeader() : void
+    {
+        $uri = 'api/login';
+        $teacher = Teacher::factory()->make();
+        $inputs = [
+            'email' => $teacher->email,
+            'password' => 'password',
+        ];
+
+        $response = $this->json('POST', $uri, $inputs);
+        $token = $this->getToken($response);
+        $this->headers = [
+            'Authorization' => "Bearer {$token}"
+        ];
+    }
+
+    public function getToken(object $response) : string
+    {
+        $content = $response->decodeResponseJson();
+        return $content['data']['token'];
+    }
 }
+
